000a3262f1209e22a90e1a8384e6256bfcf06642
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-03-11 05:08:58 +0300

    Create blog.tsx

---
+++ src/pages/blog.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Blog() {
+  return (
+    <section id="blog" className="py-20 bg-gray-950 min-h-screen">
+      <div className="container mx-auto px-4 md:px-6">
+        <h2 className="text-6xl font-permanent-marker text-center mb-10">
+          Blog Sayfası
+        </h2>
+        <p className="text-gray-300 text-center">
+          Burası yeni eklenen bir blog sayfasıdır!
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Blog;

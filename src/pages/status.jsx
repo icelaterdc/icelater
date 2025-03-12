abcefc3eb722430c0bfbc15ac99e9fa360e2d649
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-03-12 04:00:13 +0300

    Delete src/pages/status.jsx

--- src/pages/status.jsx
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    message: "Test kodu çalışıyor!",
-    status: "Başarılı",
-    data: {
-      pages: ["anasayfa", "hakkında", "iletişim"]
-    }
-  });
-}

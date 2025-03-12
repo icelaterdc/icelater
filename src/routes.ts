c0cadecf6351b9a3b823fb70d7d859a5abbfbaeb
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-03-12 03:33:44 +0300

    Create routes.ts

---
+++ src/routes.ts
@@ -0,0 +1,24 @@
+import { RouteObject } from "react-router-dom";
+
+const pageModules = import.meta.glob("./pages/**/*.{tsx,jsx}", {
+  eager: true,
+  import: "default",
+});
+
+const routes: RouteObject[] = Object.entries(pageModules).map(
+  ([filePath, component]) => {
+    const path = filePath
+      .replace("./pages/", "") // pages/ kısmını çıkar
+      .replace(/\.(tsx|jsx)$/, "") // uzantıları çıkar
+      .toLowerCase(); // küçük harfe çevir
+
+    const routePath = path === "home" ? "/" : `/${path}`;
+
+    return {
+      path: routePath,
+      element: component as React.ReactElement,
+    };
+  }
+);
+
+export default routes;

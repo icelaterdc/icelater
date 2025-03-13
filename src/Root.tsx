c7422196620a3b8a3a7f91c34e31bb17fb2a0155
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-03-13 22:14:37 +0300

    Update Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import routes from "./routes";
+import Page404 from "./pages/404"; // 404 sayfasını import ediyoruz
 
 function Root() {
   return (
@@ -15,6 +16,7 @@ function Root() {
             element={<route.element />}
           />
         ))}
+        <Route path="*" element={<Page404 />} /> {/* 404 için wildcard route */}
       </Routes>
     </BrowserRouter>
   );

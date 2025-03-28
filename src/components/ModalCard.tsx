3f66ac39b98ef143abc27197d1a67d3f84c88b47
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-03-28 04:46:39 +0300

    Update ModalCard.tsx

--- src/components/ModalCard.tsx
+++ src/components/ModalCard.tsx
@@ -21,7 +21,7 @@ const ModalCard: React.FC<ModalCardProps> = ({ imageSrc, author, description, on
       <h4 className="text-lg font-semibold mb-1">{author}</h4>
       <p className="text-gray-400 text-center mb-4">{description}</p>
       <motion.button
-        whileHover={{ scale: 1.1, borderWidth: 3 }}
+        whileHover={{ filter: 'brightness(150%)', boxShadow: '0px 0px 8px rgba(59,130,246,0.8)' }}
         onClick={onGo}
         className="px-4 py-2 border border-gray-400 bg-blue-600 text-white rounded-full transition-all duration-200"
       >

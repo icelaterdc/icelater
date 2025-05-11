549fe38af4ec553a0bf7f6cad43536ed05c847a5
IceLater <120577848+icelaterdc@users.noreply.github.com>
2025-05-11 16:38:41 +0300

    Update ActionCard.tsx

--- src/components/ActionCard.tsx
+++ src/components/ActionCard.tsx
@@ -21,7 +21,7 @@ const ActionCard: React.FC<ActionCardProps> = ({ imageSrc, author, description, 
       <motion.a
         href={link}
         className="mt-4 px-6 py-2 bg-gray-600 text-white rounded-full border-2 border-blue-500 font-medium transition-all duration-300 hover:scale-110 hover:shadow-[0_0_20px_rgba(59,130,246,0.8)] hover:border-blue-400"
-        whileHover={{ scale: 1.1 }} // Framer-motion ile büyüme
+        whileHover={{ scale: 1.1 }} 
       >
         Go
       </motion.a>
